40b7f99d262ee160433043ac80e463d898ec82c3
Rathi Saravanan (rsarvana) <rsarvana@cisco.com>
2018-01-21 14:25:30 -0800

    simplified code by replacing ifelse by for loop

--- ColorSpiral.py
+++ ColorSpiral.py
@@ -3,64 +3,13 @@
 import turtle
 t = turtle.Pen()
 turtle.bgcolor("black")
-#you can chose between 2 and 6 sides for some cool shapes!
-sides = (input("Enter a number of sides between 2 and 6:"))
+#you can chose between 2 and 8 sides for some cool shapes!
+sides = (input("Enter a number of sides between 2 and 8:"))
 sides = int(sides)
 colors = []
-
-  
-if sides == 2:
-    col = input("Enter color 1:")
+for j in range(1, sides+1):
+    col = input("Enter color"+ str(j) +":")
     colors.append(col)
-    co = input("Enter color 2:" )
-    colors.append(co)
-elif sides == 3:
-    col = input("Enter color 1:")
-    colors.append(col)
-    co = input("Enter color 2:" )
-    colors.append(co)
-    co = input("Enter color 3:" )
-    colors.append(co)
-
-elif sides == 4:
-    col = input("Enter color 1:")
-    colors.append(col)
-    co = input("Enter color 2:" )
-    colors.append(co)
-    colo = input("Enter color 3:" )
-    colors.append(colo)
-    color = input("Enter color 4:" )
-    colors.append(color)
-
-elif sides == 5:
-    col = input("Enter color 1:")
-    colors.append(col)
-    co = input("Enter color 2:" )
-    colors.append(co)
-    colo = input("Enter color 3:" )
-    colors.append(colo)
-    color = input("Enter color 4:" )
-    colors.append(color)
-    color = input("Enter color 5:" )
-    colors.append(color)
-   
-elif sides == 6:
-    col = input("Enter color 1:")
-    colors.append(col)
-    co = input("Enter color 2:" )
-    colors.append(co)
-    colo = input("Enter color 3:" )
-    colors.append(colo)
-    color = input("Enter color 4:" )
-    colors.append(color)
-    color = input("Enter color 5:" )
-    colors.append(color)
-    colorx = input("Enter color 6:" )
-    colors.append(colorx)
-
-else :
-    print("sorry, try again")
-    
 
 
 for x in range(60):
@@ -68,3 +17,7 @@ for x in range(60):
     t.forward(x*3/sides + x)
     t.left(360/sides)
     t.width(x*sides/200)
+    
+
+
+  
