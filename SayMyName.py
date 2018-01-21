a6ee0329c144dd2a458348deb188a3ae71474d03
Rathi Saravanan (rsarvana) <rsarvana@cisco.com>
2018-01-21 13:57:48 -0800

    Very easy 1st program

---
+++ SayMyName.py
@@ -0,0 +1,14 @@
+#SayMyName.py
+
+#SayMyName.py - prints a screeen full of the users name
+
+#Ask the user his/her name
+
+name = input("What's your name?")
+
+#print their name 100 times
+
+for x in range(100):
+    #print their name followed by a space, not a new line
+
+    print(name, end = " rules!")
